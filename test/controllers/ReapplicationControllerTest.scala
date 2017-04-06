1fb261df89212941aaf512063582aeea8aba2a67
David Wright <davew@MacBook-Pro.local>
2017-04-06 15:56:57 +0100

    fixed awrs-2125, awrs-2126

--- test/controllers/ReapplicationControllerTest.scala
+++ test/controllers/ReapplicationControllerTest.scala
@@ -62,15 +62,23 @@ class ReapplicationControllerTest extends AwrsUnitTestTraits
   }
 
   "Reapplication Controller" should {
-    "submit confirmation and redirect to root home page" in {
-      submitAuthorisedUser(testRequest(ReapplicationConfirmation(AWRSEnums.BooleanRadioEnum.YesString))) {
+    "submit confirmation and redirect to root home page when yes selected" in {
+      submitAuthorisedUserYes(testRequest(ReapplicationConfirmation(AWRSEnums.BooleanRadioEnum.YesString))) {
+        result =>
+          status(result) shouldBe 303
+          redirectLocation(result).get should include("/alcohol-wholesale-scheme")
+      }
+    }
+
+    "submit confirmation and redirect to root home page when no selected" in {
+      submitAuthorisedUserNo(testRequest(ReapplicationConfirmation(AWRSEnums.BooleanRadioEnum.NoString))) {
         result =>
           status(result) shouldBe 303
           redirectLocation(result).get should be("/alcohol-wholesale-scheme")
       }
     }
 
-    "show recent withdrawal error page if the user has benn Rejected within 24 hours" in {
+    "show recent withdrawal error page if the user has been Rejected within 24 hours" in {
       showWithException(testStatusNotification(StatusContactType.Rejected)) { result =>
         val document = Jsoup.parse(contentAsString(result))
         document.getElementById("application-error-header").text() should be(Messages("awrs.generic.wait_info",Messages("awrs.generic.wait_info_de-registration")))
@@ -131,7 +139,15 @@ class ReapplicationControllerTest extends AwrsUnitTestTraits
     test(result)
   }
 
-  def submitAuthorisedUser(fakeRequest: FakeRequest[AnyContentAsFormUrlEncoded])(test: Future[Result] => Any) {
+  def submitAuthorisedUserNo(fakeRequest: FakeRequest[AnyContentAsFormUrlEncoded])(test: Future[Result] => Any) {
+    setUser(hasAwrs = true)
+    setupMockSave4LaterService()
+    setupMockApiSave4LaterService()
+    val result = TestReapplicationController.submit.apply(SessionBuilder.updateRequestWithSession(fakeRequest, userId, "LTD"))
+    test(result)
+  }
+
+  def submitAuthorisedUserYes(fakeRequest: FakeRequest[AnyContentAsFormUrlEncoded])(test: Future[Result] => Any) {
     setUser(hasAwrs = true)
     setupMockSave4LaterService()
     setupMockApiSave4LaterService()
@@ -141,5 +157,4 @@ class ReapplicationControllerTest extends AwrsUnitTestTraits
     val result = TestReapplicationController.submit.apply(SessionBuilder.updateRequestWithSession(fakeRequest, userId, "LTD"))
     test(result)
   }
-
 }
