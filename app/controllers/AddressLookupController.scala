bed20ec280319326be81406e0a2d135c966df2e9
Mark Reynolds <mark.reynolds@digital.hmrc.gov.uk>
2017-11-07 09:52:34 +0000

    Sanitize post code to resolve high alert in Zap

--- app/controllers/AddressLookupController.scala
+++ app/controllers/AddressLookupController.scala
@@ -35,13 +35,19 @@ import uk.gov.hmrc.http.BadRequestException
 trait AddressLookupController extends FrontendController with Actions with HasAddressLookupService with LoggingUtils {
 
   def addressLookup(postcode: String) = AuthorisedFor(AwrsRegistrationRegime, pageVisibility = GGConfidence).async {
-    implicit user => implicit request =>
-      implicit val writes = Json.format[RecordSet]
-      addressLookupService.lookup(postcode) map {
-        case AddressLookupErrorResponse(e: BadRequestException) => BadRequest(e.message)
-        case AddressLookupErrorResponse(e) => InternalServerError
-        case AddressLookupSuccessResponse(recordSet) => Ok(writes.writes(recordSet))
-      }
+    implicit user =>
+      implicit request =>
+        implicit val writes = Json.format[RecordSet]
+        val validPostcodeCharacters = "^[A-z0-9 ]*$"
+        if (postcode.matches(validPostcodeCharacters)) {
+          addressLookupService.lookup(postcode) map {
+            case AddressLookupErrorResponse(e: BadRequestException) => BadRequest(e.message)
+            case AddressLookupErrorResponse(e) => InternalServerError
+            case AddressLookupSuccessResponse(recordSet) => Ok(writes.writes(recordSet))
+          }
+        } else {
+          Future.successful(BadRequest("missing or badly-formed postcode parameter"))
+        }
   }
 
   def addressToAuditMap(key: String, someAddress: Option[Address]): Map[String, String] =
@@ -85,35 +91,36 @@ trait AddressLookupController extends FrontendController with Actions with HasAd
 
 
   def auditAddress() = AuthorisedFor(AwrsRegistrationRegime, pageVisibility = GGConfidence).async {
-    implicit user => implicit request =>
+    implicit user =>
+      implicit request =>
 
-      val addressToAudit = request.body.asJson match {
-        case Some(js) => js.as[AddressAudits]
-        case None => AddressAudits(addressAudits = List())
-      }
+        val addressToAudit = request.body.asJson match {
+          case Some(js) => js.as[AddressAudits]
+          case None => AddressAudits(addressAudits = List())
+        }
 
-      addressToAudit.addressAudits.foreach {
-        toAudit: AddressAudit =>
-          lazy val uprn = Map(("uprn", toAudit.uprn.fold("")(x => x)))
+        addressToAudit.addressAudits.foreach {
+          toAudit: AddressAudit =>
+            lazy val uprn = Map(("uprn", toAudit.uprn.fold("")(x => x)))
 
-          toAudit.eventType match {
-            case Some(`postcodeAddressSubmitted`) =>
-              areAddressesDifferent(toAudit.toAddress, toAudit.fromAddress) match {
-                case true =>
-                  audit(transactionName = toAudit.auditPointId.get, detail = auditAddressMap(toAudit) ++ uprn, eventType = postcodeAddressModifiedSubmitted)
-                case false =>
-                  audit(transactionName = toAudit.auditPointId.get, detail = auditAddressMap(toAudit, fromAddress = false) ++ uprn, eventType = toAudit.eventType.get)
-              }
-            case Some(`manualAddressSubmitted` | `internationalAddressSubmitted`) =>
-              areAddressesDifferent(toAudit.toAddress, toAudit.fromAddress) match {
-                case true =>
-                  audit(transactionName = toAudit.auditPointId.get, detail = auditAddressMap(toAudit), eventType = toAudit.eventType.get)
-                case _ =>
-              }
-            case _ =>
-          }
-      }
-      Future.successful(Ok)
+            toAudit.eventType match {
+              case Some(`postcodeAddressSubmitted`) =>
+                areAddressesDifferent(toAudit.toAddress, toAudit.fromAddress) match {
+                  case true =>
+                    audit(transactionName = toAudit.auditPointId.get, detail = auditAddressMap(toAudit) ++ uprn, eventType = postcodeAddressModifiedSubmitted)
+                  case false =>
+                    audit(transactionName = toAudit.auditPointId.get, detail = auditAddressMap(toAudit, fromAddress = false) ++ uprn, eventType = toAudit.eventType.get)
+                }
+              case Some(`manualAddressSubmitted` | `internationalAddressSubmitted`) =>
+                areAddressesDifferent(toAudit.toAddress, toAudit.fromAddress) match {
+                  case true =>
+                    audit(transactionName = toAudit.auditPointId.get, detail = auditAddressMap(toAudit), eventType = toAudit.eventType.get)
+                  case _ =>
+                }
+              case _ =>
+            }
+        }
+        Future.successful(Ok)
   }
 
   @inline private def areAddressesDifferent(toAddress: Option[Address], fromAddress: Option[Address]) = AddressComparator.isDifferent(toAddress, fromAddress)
