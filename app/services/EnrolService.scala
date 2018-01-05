07dec07b407d0fe9ccc8c3af535f4777a79f2ccd
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2018-01-05 07:57:13 +0000

    DDCNLS-2517 Default EMAC to false

--- app/services/EnrolService.scala
+++ app/services/EnrolService.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -108,6 +108,6 @@ trait EnrolService extends RunMode with AuthorisedFunctions {
 object EnrolService extends EnrolService {
   val ggConnector = GovernmentGatewayConnector
   val taxEnrolmentsConnector: TaxEnrolmentsConnector = TaxEnrolmentsConnector
-  val isEmacFeatureToggle = runModeConfiguration.getBoolean("emacsFeatureToggle").getOrElse(true)
+  val isEmacFeatureToggle = runModeConfiguration.getBoolean("emacsFeatureToggle").getOrElse(false)
   override val authConnector = AuthClientConnector
 }
