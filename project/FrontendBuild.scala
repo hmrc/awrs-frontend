172a06aecc36dfd60fbe07c25be07f683b65d567
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2019-08-20 14:57:47 +0100

    tradingActivityFix dependency updates

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -22,7 +22,7 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "domain" % "5.6.0-play-26",
     "uk.gov.hmrc" %% "json-encryption" % "4.2.0",
     "com.mohiva" %% "play-html-compressor" % "0.6.3", // used to pretty print html by stripping out all the whitespaces added by the playframework
-    "uk.gov.hmrc" %% "auth-client" % "2.22.0-play-26",
+    "uk.gov.hmrc" %% "auth-client" % "2.27.0-play-26",
     "uk.gov.hmrc" %% "govuk-template" % "5.36.0-play-26",
     "uk.gov.hmrc" %% "play-ui" % "7.40.0-play-26"
   )
@@ -40,7 +40,7 @@ private object AppDependencies {
         "org.pegdown" % "pegdown" % "1.6.0" % scope,
         "org.jsoup" % "jsoup" % "1.8.3" % scope,
         "org.mockito" % "mockito-core" % "2.28.2" % scope,
-        "uk.gov.hmrc" %% "hmrctest" % "3.8.0-play-25" % scope,
+        "uk.gov.hmrc" %% "hmrctest" % "3.9.0-play-25" % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.0" % scope
       )
     }.test
