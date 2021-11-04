2914d7025f04c1750cb80372c146e0fbe52a248d
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2021-11-04 12:13:18 +0000

    DL-6554, more dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,8 +13,8 @@ private object AppDependencies {
     "uk.gov.hmrc"        %% "url-builder"                 % "3.5.0-play-28",
     "uk.gov.hmrc"        %% "http-caching-client"         % "9.5.0-play-28",
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-28"  % "5.16.0",
-    "uk.gov.hmrc"        %% "play-partials"               % "8.1.0-play-28", // includes code for retrieving partials, e.g. the Help with this page form
-    "uk.gov.hmrc"        %% "domain"                      % "6.1.0-play-28",
+    "uk.gov.hmrc"        %% "play-partials"               % "8.2.0-play-28", // includes code for retrieving partials, e.g. the Help with this page form
+    "uk.gov.hmrc"        %% "domain"                      % "6.2.0-play-28",
     "uk.gov.hmrc"        %% "json-encryption"             % "4.10.0-play-28",
     "com.yahoo.platform.yui" % "yuicompressor" % "2.4.8",
     "com.mohiva"         %% "play-html-compressor"        % "0.7.1", // used to pretty print html by stripping out all the whitespaces added by the playframework
@@ -36,9 +36,8 @@ private object AppDependencies {
         "org.pegdown"             % "pegdown"                 % "1.6.0" % scope,
         "org.jsoup"               % "jsoup"                   % "1.14.3" % scope,
         "org.mockito"            % "mockito-core"             % "4.0.0" % scope,
-        "org.mockito"            %% "mockito-scala"           % "1.16.42" % scope,
-        "org.mockito"            %% "mockito-scala-scalatest" % "1.16.42" % scope,
-        "org.mockito"             % "mockito-core"            % "3.12.4" % scope,
+        "org.mockito"            %% "mockito-scala"           % "1.16.46" % scope,
+        "org.mockito"            %% "mockito-scala-scalatest" % "1.16.46" % scope,
         "org.scalatestplus.play"  %% "scalatestplus-play"     % scalaTestplusPlayVersion % scope,
         "org.scalatestplus"       %% "scalatestplus-mockito"        % "1.0.0-M2",
         "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2" % "test"
@@ -57,7 +56,7 @@ private object AppDependencies {
         "com.typesafe.play"       %% "play-test"           % PlayVersion.current % scope,
         "org.scalatestplus.play"  %% "scalatestplus-play"  % scalaTestplusPlayVersion % scope,
         "com.github.tomakehurst"       %  "wiremock-jre8"           % "2.31.0",
-        "com.fasterxml.jackson.module" %% "jackson-module-scala"    % "2.12.5",
+        "com.fasterxml.jackson.module" %% "jackson-module-scala"    % "2.13.0",
         "org.scalatestplus"       %% "scalatestplus-mockito"        % "1.0.0-M2",
         "org.scalatestplus" %% "scalatestplus-scalacheck" % "3.1.0.0-RC2" % "test"
       )
