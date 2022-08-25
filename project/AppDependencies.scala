520191eaa45513bdee54e20f4c9cf55f7d67cce7
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2022-08-25 15:25:37 +0100

    DL-8369 dependency and plugin updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,7 +18,7 @@ private object AppDependencies {
     "uk.gov.hmrc"            %% "json-encryption"            % "4.11.0-play-28",
     "com.yahoo.platform.yui" %  "yuicompressor"              % "2.4.8",
     "com.mohiva"             %% "play-html-compressor"       % "0.7.1", // used to pretty print html by stripping out all the whitespaces added by the playframework
-    "uk.gov.hmrc"            %% "play-frontend-hmrc"         % "3.22.0-play-28",
+    "uk.gov.hmrc"            %% "play-frontend-hmrc"         % "3.23.0-play-28",
     "com.typesafe.play"      %% "play-json-joda"             % "2.9.2"
   )
 
@@ -34,8 +34,8 @@ private object AppDependencies {
       override lazy val test = Seq(
         "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "5.25.0"                 % scope,
         "org.pegdown"            %  "pegdown"                  % "1.6.0"                  % scope,
-        "org.jsoup"              %  "jsoup"                    % "1.15.2"                 % scope,
-        "org.mockito"            %  "mockito-core"             % "4.6.1"                  % scope,
+        "org.jsoup"              %  "jsoup"                    % "1.15.3"                 % scope,
+        "org.mockito"            %  "mockito-core"             % "4.7.0"                  % scope,
         "org.scalatestplus"      %% "scalatestplus-mockito"    % "1.0.0-M2"               % scope
       )
     }.test
