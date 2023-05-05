6731509ad2b87c4dc5f7f27dab1381adc1d414c4
nadinJ <63252254+nadinJ@users.noreply.github.com>
2023-05-05 15:48:40 +0100

    DL-10190: Fixed html validation errors on status pages

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,7 +13,7 @@ private object AppDependencies {
     "uk.gov.hmrc"            %% "domain"                     % "8.2.0-play-28",
     "com.yahoo.platform.yui" %  "yuicompressor"              % "2.4.8",
     "com.mohiva"             %% "play-html-compressor"       % "0.7.1", // used to pretty print html by stripping out all the whitespaces added by the playframework
-    "uk.gov.hmrc"            %% "play-frontend-hmrc"         % "7.3.0-play-28",
+    "uk.gov.hmrc"            %% "play-frontend-hmrc"         % "7.7.0-play-28",
     "com.typesafe.play"      %% "play-json-joda"             % "2.9.4"
   )
 
@@ -27,7 +27,7 @@ private object AppDependencies {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val test = Seq(
         "uk.gov.hmrc"       %% "bootstrap-test-play-28"   % "5.25.0"   % scope,
-        "org.jsoup"         %  "jsoup"                    % "1.15.4"   % scope,
+        "org.jsoup"         %  "jsoup"                    % "1.16.1"   % scope,
         "org.mockito"       %  "mockito-core"             % "4.11.0"   % scope,
         "org.scalatestplus" %% "scalatestplus-mockito"    % "1.0.0-M2" % scope
       )
@@ -43,7 +43,7 @@ private object AppDependencies {
         "uk.gov.hmrc"                  %% "bootstrap-test-play-28"   % "5.25.0"                 % scope,
         "com.typesafe.play"            %% "play-test"                % PlayVersion.current      % scope,
         "com.github.tomakehurst"       %  "wiremock-jre8"            % "2.35.0"                 % scope,
-        "com.fasterxml.jackson.module" %% "jackson-module-scala"     % "2.14.2"                 % scope
+        "com.fasterxml.jackson.module" %% "jackson-module-scala"     % "2.15.0"                 % scope
       )
     }.test
   }
