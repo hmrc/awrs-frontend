5fd6f96cd44ee2722fee46633c8e3d2d47da4b7c
nehakannaujia1 <50657531+nehakannaujia1@users.noreply.github.com>
2025-01-22 22:18:30 +0000

    Added pages and test

---
+++ app/controllers/URNKickOutController.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+
+import audit.Auditable
+import config.ApplicationConfig
+import controllers.auth.AwrsController
+import play.api.mvc.{AnyContent, _}
+import services.{DeEnrolService, Save4LaterService}
+import uk.gov.hmrc.play.bootstrap.auth.DefaultAuthConnector
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import utils.AccountUtils
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class URNKickOutController @Inject()(val mcc: MessagesControllerComponents,
+                                       implicit val applicationConfig: ApplicationConfig,
+                                       template: views.html.urn_kickout) extends FrontendController(mcc) {
+
+ 
+  val signInUrl: String = applicationConfig.signIn
+  implicit val ec: ExecutionContext = mcc.executionContext
+
+  def showURNKickOutPage() : Action[AnyContent] = Action.async { implicit request =>
+       Future.successful(Ok(template()))
+   }
+}
---
+++ test/controllers/URNKickOutControllerTest.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import builders.SessionBuilder
+import play.api.mvc.{AnyContentAsEmpty, AnyContentAsFormUrlEncoded, Result}
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import services.DataCacheKeys._
+import services.ServicesUnitTestFixture
+import utils.AwrsUnitTestTraits
+import views.html.urn_kickout
+
+import scala.concurrent.Future
+
+class URNKickOutControllerTest extends AwrsUnitTestTraits
+  with ServicesUnitTestFixture {
+
+  val request: FakeRequest[AnyContentAsEmpty.type] = FakeRequest()
+  val template: urn_kickout = app.injector.instanceOf[views.html.urn_kickout]
+
+  val testURNKickOutController: URNKickOutController = new URNKickOutController( mockMCC, mockAppConfig, template)
+
+  "URNKickOutController" must {
+
+    "show the Kickout page" in {
+            val res = testURNKickOutController.showURNKickOutPage().apply(SessionBuilder.buildRequestWithSession(userId))
+              status(res) mustBe 200
+        }
+  }
+
+}
