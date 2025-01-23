06c54926c6c47a06729c70d729b332ddfdc1fb50
Byasar3 <byasar3@users.noreply.github.com>
2025-01-23 15:20:36 +0000

    wip

--- app/services/KeyStoreService.scala
+++ app/services/KeyStoreService.scala
@@ -142,4 +142,9 @@ class KeyStoreService @Inject()(keyStoreConnector: AwrsKeyStoreConnector) {
     keyStoreConnector.fetchDataFromKeystore[CacheMap](save4LaterBackupName)
 
   @inline def removeAll(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Unit] = keyStoreConnector.removeAll()
+
+  @inline def saveHaveYouRegistered(haveYouRegisteredModel: HaveYouRegisteredModel)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[CacheMap] =
+    keyStoreConnector.saveDataToKeystore[HaveYouRegisteredModel](enrolmentJourneyHaveYouRegistered, haveYouRegisteredModel)
+
+  @inline def fetchHaveYouRegistered(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Option[HaveYouRegisteredModel]] = keyStoreConnector.fetchDataFromKeystore(enrolmentJourneyHaveYouRegistered)
 }
