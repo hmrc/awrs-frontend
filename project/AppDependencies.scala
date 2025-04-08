7f6d4f35f74ec22cf8b08b1a824de15b8bd783c0
Preetam Potdar <10691333+preetampotdar@users.noreply.github.com>
2025-04-08 07:53:06 +0100

    chore(DL-15638): Removed legacy auth configuration (#369)
    * chore(DL-15638): Removed legacy auth configuration
    
    * chore(DL-15638): Updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import sbt.*
 import play.sbt.PlayImport.*
 
@@ -12,15 +28,15 @@ private object AppDependencies {
     "uk.gov.hmrc"                   %% "play-partials-play-30"       % "10.0.0", // includes code for retrieving partials, e.g. the Help with this page form
     "uk.gov.hmrc"                   %% "domain-play-30"              % "10.0.0",
     "com.yahoo.platform.yui"        %  "yuicompressor"               % "2.4.8",
-    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"  % "11.12.0",
+    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"  % "11.13.0",
     "commons-codec"                 %  "commons-codec"               % "1.18.0",
     "com.googlecode.htmlcompressor" %  "htmlcompressor"              % "1.5.2"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapPlayVersion % Test,
-    "org.jsoup"         %  "jsoup"                   % "1.18.3"             % Test,
-    "org.mockito"       %  "mockito-core"            % "5.15.2"             % Test,
+    "org.jsoup"         %  "jsoup"                   % "1.19.1"             % Test,
+    "org.mockito"       %  "mockito-core"            % "5.17.0"             % Test,
     "org.scalatestplus" %% "mockito-5-12"            % "3.2.19.0"           % Test
   )
 
