aa117f20319ab17608ef285f69189a47c413932c
Gaurav Sood <88654936+daglisood@users.noreply.github.com>
2025-06-23 15:58:15 +0100

    DL-16683: Removing PII from logs (#389)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -23,11 +23,11 @@ private object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"                   %% "http-caching-client-play-30" % "12.1.0",
+    "uk.gov.hmrc"                   %% "http-caching-client-play-30" % "12.2.0",
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30"  % bootstrapPlayVersion,
-    "uk.gov.hmrc"                   %% "play-partials-play-30"       % "10.0.0", // includes code for retrieving partials, e.g. the Help with this page form
+    "uk.gov.hmrc"                   %% "play-partials-play-30"       % "10.1.0", // includes code for retrieving partials, e.g. the Help with this page form
     "com.yahoo.platform.yui"        %  "yuicompressor"               % "2.4.8",
-    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"  % "12.5.0",
+    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"  % "12.6.0",
     "commons-codec"                 %  "commons-codec"               % "1.18.0",
     "com.googlecode.htmlcompressor" %  "htmlcompressor"              % "1.5.2"
   )
