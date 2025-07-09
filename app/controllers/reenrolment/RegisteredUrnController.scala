53c71eeba667e9cab93d5305f8be8dae544767b2
Michael Olafisoye <molafisoye@gmail.com>
2025-07-09 13:39:57 +0100

    DL-16749 print details for all test URNS

--- app/controllers/reenrolment/RegisteredUrnController.scala
+++ app/controllers/reenrolment/RegisteredUrnController.scala
@@ -49,6 +49,49 @@ class RegisteredUrnController @Inject() (mcc: MessagesControllerComponents,
   implicit val ec: ExecutionContext = mcc.executionContext
   val signInUrl: String             = applicationConfig.signIn
 
+  val validIds: List[String] = List(
+    "XLAW00000200190",
+    "XYAW00000200191",
+    "XRAW00000200192",
+    "XZAW00000200193",
+    "XXAW00000200188",
+    "XXAW00000200295",
+    "XFAW00000200294",
+    "XYAW00000200297",
+    "XLAW00000200296",
+    "XPAW00000200123",
+    "XFAW00000200111",
+    "XXAW00000200112",
+    "XYAW00000200114",
+    "XRAW00000200115",
+    "XZAW00000200116",
+    "XAAW00000200117",
+    "XCAW00000200135",
+    "XDAW00000200118",
+    "XGAW00000200090",
+    "XJAW00000200091",
+    "XYAW00000200084",
+    "XBAW00000200101",
+    "XEAW00000200102",
+    "XQAW00000200106",
+    "XTAW00000200107",
+    "XHAW00000200128",
+    "XCAW00000200110",
+    "XVAW00000200125",
+    "XEAW00000200127",
+    "XWAW00000200134",
+    "XPAW00000200093",
+    "XSAW00000200094",
+    "XVAW00000200095",
+    "XKAW00000200129",
+    "XKAW00000200130",
+    "XQAW00000200132",
+    "XGAW00000200119",
+    "XEAW00000200097",
+    "XHAW00000200098",
+    "XKAW00000200099"
+  )
+
   def showArwsUrnPage(): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
     enrolmentEligibleAuthorisedAction { implicit ar =>
       restrictedAccessCheck {
@@ -71,11 +114,11 @@ class RegisteredUrnController @Inject() (mcc: MessagesControllerComponents,
             .fold(
               formWithErrors => Future.successful(BadRequest(template(formWithErrors))),
               awrsUrn => {
-                val es20Response = enrolmentsConnector.lookupEnrolments(KnownFacts(urn = awrsUrn.awrsUrn))
-                es20Response.foreach { resp =>
-                  logger.info(s"es20 response $resp")
+                validIds.map { ids =>
+                  val es20Resp = enrolmentsConnector.lookupEnrolments(KnownFacts(urn = ids))
+                  es20Resp.map(resp => logger.info(s"es20 response for $ids - $resp"))
                 }
-                Future.successful(Ok(es20Response.toString))
+                Future.successful(Ok("Done"))
               }
             )
         } else {
